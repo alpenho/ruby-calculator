782b63101bd7593834331bb5a393de19ea264bd6
Muhammad Alpen Saputra Halim <alpensaputra@outlook.com>
2023-10-02 18:08:27 +0700

    Put the calculator command as a parent class to Ruby calculator and then adding method command and convert_to_float

--- lib/ruby_calculator.rb
+++ lib/ruby_calculator.rb
@@ -1,8 +1,22 @@
-class RubyCalculator
+class RubyCalculator < CalculatorCommand
   attr_accessor :result, :history
 
   def initialize
-    @result = 0
+    @result = 0.0
     @history = []
+  end
+
+  def command(command_string)
+    @history << command_string
+    inputs = command_string.split(' ')
+    operation = inputs.shift
+    convert_to_float!(inputs).unshift(@result)
+
+    super operation, inputs
+  end
+
+  def convert_to_float!(strings)
+    return [] if strings.empty?
+    strings.map!(&:to_f)
   end
 end
--- spec/ruby_calculator_spec.rb
+++ spec/ruby_calculator_spec.rb
@@ -1,14 +1,29 @@
-require 'ruby_calculator'
-
 describe RubyCalculator do
   context 'initialize' do
-    it 'should have result number zero' do
-      expect(subject.result).to eql(0)
+    it 'should have result decimal zero' do
+      expect(subject.result).to eql(0.0)
     end
 
     it 'should have empty history array command' do
       expect(subject.history).to be_an_instance_of(Array)
       expect(subject.history).to be_empty
+    end
+  end
+
+  context 'command' do
+    it 'should put command string into history array' do
+      expect(subject.command('add 3')).to eql(3.0)
+      expect(subject.history).to eql(['add 3'])
+    end
+  end
+
+  context 'convert_to_float' do
+    it 'should return list of float number' do
+      expect(subject.convert_to_float!(['1', '2.4', '3'])).to eql([1.0, 2.4, 3.0])
+    end
+
+    it 'should return empty list when arguments is empty list' do
+      expect(subject.convert_to_float!([])).to eql([])
     end
   end
 end

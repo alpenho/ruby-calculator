c654d8044fe683fd145b5cf0ff9b932e5a6682dc
Muhammad Alpen Saputra Halim <alpensaputra@outlook.com>
2023-10-02 19:28:17 +0700

    Implement command repeat

--- lib/ruby_calculator.rb
+++ lib/ruby_calculator.rb
@@ -6,13 +6,30 @@ class RubyCalculator < CalculatorCommand
     @history = []
   end
 
-  def command(command_string)
-    @history << command_string
+  def command(command_string, repeat_index: nil)
+    # put the command string to history if this command is being run by user, not by repeat
+    @history << command_string if repeat_index.nil?
     inputs = command_string.split(' ')
     operation = inputs.shift
-    convert_to_float!(inputs).unshift(@result)
 
+    if operation == 'repeat'
+      return repeat(inputs.first.to_i, repeat_index)
+    end
+
+    convert_to_float!(inputs).unshift(@result)
     super operation, inputs
+  end
+
+  def repeat(number, repeat_index)
+    repeat_index = @history.length - 1 if repeat_index.nil?
+    start_index = repeat_index - number
+
+    @history[start_index..repeat_index - 1].each do |command_string|
+      command(command_string, repeat_index: start_index)
+      start_index += 1
+    end
+
+    @result
   end
 
   def convert_to_float!(strings)
--- spec/ruby_calculator_spec.rb
+++ spec/ruby_calculator_spec.rb
@@ -15,6 +15,30 @@ describe RubyCalculator do
       expect(subject.command('add 3')).to eql(3.0)
       expect(subject.history).to eql(['add 3'])
     end
+
+    context 'repeat' do
+      it 'should calculate correctly' do
+        expect(subject.command('add 3')).to eql(3.0)
+        expect(subject.command('add 5')).to eql(8.0)
+        expect(subject.command('repeat 2')).to eql(16.0)
+      end
+
+      it 'should not put command string into history array' do
+        expect(subject.command('add 3')).to eql(3.0)
+        expect(subject.command('add 5')).to eql(8.0)
+        expect(subject.command('repeat 2')).to eql(16.0)
+        expect(subject.history).to eql(['add 3', 'add 5', 'repeat 2'])
+      end
+
+      it 'should calculate correctly with multiple repeat' do
+        expect(subject.command('add 3')).to eql(3.0)
+        expect(subject.command('add 5')).to eql(8.0)
+        expect(subject.command('repeat 2')).to eql(16.0)
+        expect(subject.command('add 2')).to eql(18.0)
+        expect(subject.command('repeat 3')).to eql(33.0)
+        expect(subject.command('repeat 4')).to eql(63.0)
+      end
+    end
   end
 
   context 'convert_to_float' do

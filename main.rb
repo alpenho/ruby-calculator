bcba78d7070870e201dfabcdfefd0104fd0d7c47
Muhammad Alpen Saputra Halim <alpensaputra@outlook.com>
2023-10-03 13:41:40 +0700

    Put all the command available into the first text when run the app

--- main.rb
+++ main.rb
@@ -5,6 +5,18 @@ This is a ruby calculator, at first it will zero.
 The format is like this: <command><space><number> (some command doesn't need number)
 Please type in these commands:
 1. add<space><number> (Any number, integer or decimal)
+2. subtract<space><number> (Any number, integer or decimal)
+3. multiply<space><number> (Any number, integer or decimal)
+4. divide<space><number> (Any number, integer or decimal)
+5. abs (make the result number absolute)
+6. neg (negate the result number)
+7. sqrt (square root the result number)
+8. sqr (raise the result number to the power of 2)
+9. cubert (cube root the result number)
+10. cube (raise the result number to the power of 3)
+11. repeat<space><step> (repeat operation from n-step before)
+12. cancel (reset the calculator to 0.0)
+13. exit (exit the program)
 TEXT
 
 calculator = RubyCalculator.new
